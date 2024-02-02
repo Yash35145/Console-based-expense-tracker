fec85561044db5cf4bb563395f60f29ec75de09b
Yash <yashsanodiya169@gmail.com>
2024-02-03 02:26:00 +0530

    Added functionality

--- main.java
+++ main.java
@@ -107,9 +107,56 @@ class expenseStorage{
 
 }
 
+class categories{
+
+    static void storeCategories(ArrayList <String> categories){
+        File categoriesFile = new File("categories.txt");
+        if(categoriesFile.exists()){}
+        else{
+            try{
+                categoriesFile.createNewFile();
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+        try {
+            FileWriter categoriesFileWriter = new FileWriter("categories.txt");
+            for (String category : categories) {
+                categoriesFileWriter.write(category.toLowerCase() + ",");
+            }
+            categoriesFileWriter.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    static ArrayList<String> readCategories(){
+        ArrayList <String> categories = new ArrayList<>();
+        File categoriesFile = new File("categories.txt");
+        if(categoriesFile.exists()){
+            try {
+                Scanner sc = new Scanner(categoriesFile);
+                while(sc.hasNextLine()){
+                    String line = sc.nextLine();
+                    String[] categoryArr = line.split(",");
+                    for (String string : categoryArr) {
+                        categories.add(string);
+                    }
+                }
+                sc.close();            
+            } 
+                catch (Exception e) {
+                    System.out.println("Cannot retrieve categories from file.");
+                }
+        }
+        return categories;
+    }
+
+}
+
 public class main{
     static void waitForEnter(){
-        System.out.println("Press enter to continue..");
+        System.out.println("\nPress enter to continue..");
         try{System.in.read();}
         catch(Exception e){}
     }
@@ -120,7 +167,8 @@ public class main{
     }
 
     public static void main(String[] args) {
-
+        // arrayList to store categories 
+        ArrayList<String> categoriesList = categories.readCategories();
         // arraylist to store object of class expense
         ArrayList<expense> expObjects = expenseStorage.readExpense();
         Scanner sc = new Scanner(System.in);
@@ -198,6 +246,79 @@ public class main{
                 waitForEnter();
                 break;
 
+                case 3:
+                clearScreen();
+                System.out.println("\n*****************");
+                System.out.println("Manage Categories");
+                System.out.println("*****************\n");
+                System.out.println("1) Add category");
+                System.out.println("2) View categories");
+                System.out.println("3) Delete category");
+                int categoryChoice = 0;
+                System.out.print("Enter your choice : ");
+                categoryChoice = sc.nextInt();
+                sc.nextLine();
+                switch (categoryChoice) {
+                    case 1:
+                    clearScreen();
+                    System.out.println("\n**************");
+                    System.out.println("Add Category");
+                    System.out.println("**************\n");
+                        System.out.print("\nEnter the name of the category :");
+                        String categoryName = sc.nextLine();
+                        categoriesList.add(categoryName);
+                        try {
+                            categories.storeCategories(categoriesList);
+                            System.out.println("\nCategory added successfully");
+                        } catch (Exception e) {
+                            System.out.println("Error occured while storing categories");
+                        }
+                        waitForEnter();
+                        break;
+                    case 2:
+                    clearScreen();
+                    System.out.println("\n****************");
+                    System.out.println("View Categories");
+                    System.out.println("****************\n");
+                    int i = 1;
+                        System.out.println(""); // for gap between line to avoid confusion
+                        for (String seperateCategory : categoriesList) {
+                            System.out.println(i+")"+seperateCategory);
+                            i++;
+                        }
+                        waitForEnter();
+                    break;
+
+                    case 3:    
+                    clearScreen();
+                    System.out.println("\n**************");
+                    System.out.println("Delete Category");
+                    System.out.println("**************\n");
+                    System.out.println("Disclaimer :- Make sure the category name must be correct.");
+                    System.out.print("\nEnter the category name to be deleted:");
+                    String deleteCategory = sc.nextLine();
+                    boolean categoryDeleted = false;
+                    for (String seperateCategory : categoriesList) {
+                        if(deleteCategory.toLowerCase().equalsIgnoreCase(seperateCategory)){
+                            categoriesList.remove(categoriesList.indexOf(deleteCategory));
+                            categoryDeleted = true;
+                        }
+                    }
+                    if(categoryDeleted){
+                        System.out.println("\nCategory deleted successfully");
+                    }else{
+                        System.out.println("\nCategory not found");
+                    }
+                    categories.storeCategories(categoriesList);
+                    waitForEnter();
+                    break;
+
+                    default:
+                        System.out.println("Invalid Choice!");
+                }
+                waitForEnter();
+                break;
+
                 case 4:
                 clearScreen();
                 System.out.println("\n\n***************");
@@ -209,6 +330,61 @@ public class main{
                     i++;
                 }
                 System.out.println("\n");
+                waitForEnter();
+                break;
+
+                case 5 :
+                clearScreen();
+                System.out.println("\n\n******************");
+                System.out.println("Modify Expense");
+                System.out.println("******************\n");
+                System.out.print("Enter Expense ID to modify expense :");
+                int modifyExpId = sc.nextInt();
+                expense modifyExpense = new expense();
+                boolean expenseFound = false;
+                try {
+                    for (expense seperateExpense : expObjects) {
+                        if(seperateExpense.getExp_id() == modifyExpId){
+                            modifyExpense = seperateExpense;
+                            expObjects.remove(expObjects.indexOf(seperateExpense));
+                            expenseFound = true;
+                        }
+                    }
+                } catch (Exception e) {
+                    // System.out.println("Cannot retrive expense for modification");
+                }
+                
+                if(expenseFound){
+                    System.out.print("\nEnter amount :");
+                    int newAmount = sc.nextInt();
+                    sc.nextLine();
+                    System.out.print("Enter category :");
+                    String newCategory = sc.nextLine();
+                    System.out.print("Enter description :");
+                    String newDescription = sc.nextLine();
+                    System.out.print("Enter date (dd/mm/yyyy) :");
+                    String newDate = sc.nextLine();
+                    modifyExpense.amount = newAmount;
+                    modifyExpense.category = newCategory;
+                    modifyExpense.description = newDescription;
+                    modifyExpense.date = newDate;
+
+                    try {
+                        expObjects.add(modifyExpense);
+                        System.out.println("\nExpense modified successfully....");
+                    } catch (Exception e) {
+                        System.out.println("\nCannot modify expense...");
+                    }
+                }
+                else{
+                    System.out.println("\nExpense not found. Try again...");
+                }
+                expenseStorage.storeExpense(expObjects);
+                waitForEnter();
+                break;
+
+                case 6 :
+                System.out.println("Currently under Development... (don't know when it will finish....)");
                 waitForEnter();
                 break;
 
